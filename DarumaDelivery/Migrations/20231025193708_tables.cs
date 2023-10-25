f0d61de68bd85aebb7148d4b1803459dd5bcc921
Jacky Zhong <ac111517@avcol.school.nz>
2023-10-26 08:38:32 +1300

    fixed tables

---
+++ DarumaDelivery/Migrations/20231025193708_tables.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DarumaDelivery.Migrations
+{
+    public partial class tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductGroup");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RegisterID",
+                table: "Customer",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RegisterID",
+                table: "Customer",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "ProductGroup",
+                columns: table => new
+                {
+                    ProductGroupID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductGroupDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductGroupTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductGroup", x => x.ProductGroupID);
+                });
+        }
+    }
+}
